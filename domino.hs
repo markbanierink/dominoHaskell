b9a92dd90362224a85a3fdae573da66eeb551f71
Mark Banierink <mark.banierink@nedap.com>
2017-10-09 09:44:49 +0200

    occurrenes switched from Bone to Pos

--- domino.hs
+++ domino.hs
@@ -17,12 +17,16 @@ cls = putStr "\ESC[2J" -- control character for clearing screen
 -- defining Grid as a list of list of Ints
 type Grid = [[Int]]
 
+-- defining a location as a tuple with two points (Int, Int)
+type Loc = (Int, Int)
+
 -- defining a Bone as a tuple of two Ints
 type Bone = (Int, Int)
 
--- defining a location as a tuple with two positions (Int, Int) and a possible Bone
-type Loc = (Int, Int)
+-- defining a Position as a combination of a Location and the fitting Bone
+type Pos = (Loc, Bone)
 
+-- the maximum value in the game
 maxBone :: Int
 maxBone = 6
 
@@ -54,12 +58,14 @@ interleave x []     = []
 interleave x [y]    = [y]
 interleave x (y:ys) = y : x : interleave x ys
 
-
-
+-- chop List of Ints into Grid
+chop :: Int -> [Int] -> Grid
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
 
 -- determine all location combinations on the grid
-locsBones :: Grid -> [Loc]
-locsBones g = concat [locations (transpose g) 1, locations g (width g)]
+initialLocs :: Grid -> [Loc]
+initialLocs g = concat [locations (transpose g) 1, locations g (width g)]
 
 locations :: Grid -> Int -> [Loc]
 locations g p2start = zip p1 p2
@@ -72,10 +78,10 @@ width :: Grid -> Int
 width g = length (head g)
 
 -- determine all possible bones on the grid
-possBones :: Grid -> [Bone]
-possBones g = concat [map orderBone (zips (transpose g)), map orderBone (zips g)]
+initialBones :: Grid -> [Bone]
+initialBones g = concat [map orderBone (zips (transpose g)), map orderBone (zips g)]
 
--- create bone for each horizontal neighbour
+-- create bone for each vertical neighbour
 zips :: Grid -> [Bone]
 zips [x] = []
 zips g   = concat [row, rest]
@@ -88,30 +94,55 @@ orderBone :: Bone -> Bone
 orderBone (b1, b2) | b1 <= b2  = (b1, b2)
                    | otherwise = (b2, b1)
 
+-- turn initial grid into map of locations and bones
+initial :: Grid -> [Pos]
+initial g = zip (initialLocs g) (initialBones g)
 
-
-
-
-
--- determine the number of a bone           ORDERING MIGHT BE DONE OUTSIDE
+-- determine the number of a bone
 boneNum :: Bone -> Int
 boneNum (b1, b2) = sum [maxBone+1-b1..maxBone] + b2 + 1
 
-
-
-
-
 -- lists the number of occurences of all possible bones
-occurences :: [Bone] -> [[Int]]
-occurences possBones = [elemIndices u possBones | u <- nub possBones]     
+occurences :: [Pos] -> [[Int]]
+occurences ps = [elemIndices u possBones | u <- nub possBones]
+                 where possBones = [snd p | p <- ps]
 
-sortLL :: [[Int]] -> [[Int]]
-sortLL list = sortBy (comparing length) list
+-- sort a list of lists by length
+sortByLength :: [[Int]] -> [[Int]]
+sortByLength list = sortBy (comparing length) list
+
+
+-- play :: Grid -> IO ()
+-- play g | length indices == 1    = putStrLn ("Er is een unieke op: " ++ show (initialLocs g !! head(indices)))
+--        | otherwise              = putStrLn "Branchen met die handel"
+--          where indices = head (sortByLength (occurences (initialBones g)))
+
+play :: Grid -> IO ()
+play g | length indices == 1    = putStrLn ("Er is een unieke op: " ++ show (initial g !! head(indices)))
+       | otherwise              = putStrLn "Branchen met die handel"
+         where indices = head (sortByLength (occurences (initial g)))
+
+
+-- run :: [Pos] -> [Int]
+-- run ps = 
+
+-- play' :: [Pos] -> [Pos]
+
+-- neighbours can be found by looking for overlapping positions.
+-- if a possible bone has no overlapping positions with other possible bones, it has no neighbours
 
 -- [[1,2,3],[3,4],[3,4,5,6]]
 -- [(0,1),(0,1),(3,4),(5,6)]
+-- putGrid (chop (width startGrid) (concat startGrid))
 
-play :: Grid -> IO ()
-play g | length indices == 1    = putStrLn ("Er is een unieke op: " ++ show (locsBones g !! head(indices)))
-       | otherwise              = putStrLn "Branchen met die handel"
-         where indices = head (sortLL (occurences (possBones g)))
+
+-- filteredPossBones :: [Pos] -> Loc -> [Pos]
+-- filteredPossBones ((l1,l2),(b1,b2)) (r1,r2) = filter (possBoneFilter (l1,l2) (r1,r2)) ((l1,l2),(b1,b2))
+filteredPossBones :: [Pos] -> Loc -> [Pos]
+filteredPossBones ps r = [p | p <- ps, possBoneFilter p r]
+
+possBoneFilter :: Pos -> Loc -> Bool
+possBoneFilter (ls,bs) (r1,r2) = l1 /= r1 && l2 /= r1 && l1 /= r2 && l2 /= r2
+                                 where
+                                    l1 = fst ls
+                                    l2 = snd ls
