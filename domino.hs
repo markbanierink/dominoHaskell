bcbc09e99a994cfe0a4a55b9f2106ee49dd37838
Mark Banierink <mark.banierink@nedap.com>
2017-10-09 15:45:03 +0200

    new filtering, not implemented

--- domino.hs
+++ domino.hs
@@ -100,9 +100,6 @@ orderBone :: Bone -> Bone
 orderBone (b1, b2) | b1 <= b2  = (b1, b2)
                    | otherwise = (b2, b1)
 
-
-
-
 -- determine the number of a bone
 boneNum :: Bone -> Int
 boneNum (b1, b2) = sum [boneNums-b1..boneNums-1] + b2 + 1
@@ -111,32 +108,6 @@ boneNum (b1, b2) = sum [boneNums-b1..boneNums-1] + b2 + 1
 numBones :: Int -> Int
 numBones n = n * (n + 1) `div` 2
 
--- lists the number of occurences of all possible bones
-occurences :: [Pos] -> [[Int]]
-occurences ps = [elemIndices u possBones | u <- nub possBones]
-                 where possBones = [snd p | p <- ps]
-
--- sort a list of lists by length
-sortByLength :: [[Int]] -> [[Int]]
-sortByLength list = sortBy (comparing length) list
-
-
-
-
-
-
--- play :: Grid -> IO ()
--- play g = do cls
---             goto (1,1)
---             putGrid g
---             solve (initial g)
-
-
-
--- play' :: [Pos] -> IO ()
--- play' ps | length indices == 1 = putStrLn ("Er is een unieke op: " ++ show (ps !! head indices)) -- play' (filteredPossBones )
---          | otherwise           = putStrLn "Branchen met die handel"
---            where indices = head (sortByLength (occurences ps))
 
 
 
@@ -153,30 +124,53 @@ play' ps w = printSolutions (solutions2Grids (solve ps []) w)
 
 solve :: [Pos] -> [Sol] -> [Sol]
 solve ps gs | length gs == numBones boneNums = gs
-            | length indices == 1            = solve (filterUniques ps (fst (ps !! head indices))) gs
-            | length neighbours == 1         = solve (filterNeighbours ps (1,1)) gs
-            | otherwise                      = []
+            | length indices == 1            = solve (filterLocations ps (fst (ps !! head indices))) gs
+            | length neighbour == 1          = solve (filterNeighbours ps (1,1)) gs
+            | otherwise                      = [] -- branchen!!!
               where
-                indices    = head (sortByLength (occurences ps))
-                neighbours = [5]
+                indices    = uniques (occurences ps)
+                neighbour  = [5]
+
+-- lists the number of occurences of all possible bones
+occurences :: [Pos] -> [[Int]]
+occurences ps = [elemIndices u possBones | u <- nub possBones]
+                 where possBones = [snd p | p <- ps]
+
+-- -- sort a list of lists by length
+-- sortByLength :: [[Int]] -> [[Int]]
+-- sortByLength list = sortBy (comparing length) list
+
+-- search list for uniques
+uniques :: [[Int]] -> [Int]
+uniques occs = [head x | x <- occs, length x == 1]
+
+occurOnce :: [Int] -> [Int]
+occurOnce xs = [x | x <- xs, count x xs == 1]
+
+count :: Int -> [Int] -> Int
+count x xs =  length (filter (==x) xs)
+
+locIndices :: [Pos] -> [Int]
+locIndices ps = concat (map (\(x,y) -> [x,y]) ls)
+                where ls = [l | (l,b) <- ps]
+
+-- filters a list of positions with a list of location indices
+indexFilter :: [Pos] -> [Int] -> [Pos]
+indexFilter ps ls = filter ((\(x,y) -> x /= l && y /= l).fst) (indexFilter ps (tail ls))
+                    where l = head ls
+
+-- uni :: [(Int, Int)] -> [Int]
+-- uni (ls,bs) = concat (map (\(x,y) -> [x,y]) ls)
 
 
--- neighbours can be found by looking for overlapping positions.
--- if a possible bone has no overlapping positions with other possible bones, it has no neighbours
+-- (initial startGrid) !! (head (uniques (occurences (initial startGrid))))
 
--- [[1,2,3],[3,4],[3,4,5,6]]
--- [(0,1),(0,1),(3,4),(5,6),(3,5)]
--- putGrid (chop (width startGrid) (concat startGrid))
-
-
--- filteredPossBones :: [Pos] -> Loc -> [Pos]
--- filteredPossBones ((l1,l2),(b1,b2)) (r1,r2) = filter (possBoneFilter (l1,l2) (r1,r2)) ((l1,l2),(b1,b2))
 -- Filters positions leaving locations given as an argument out
-filterUniques :: [Pos] -> Loc -> [Pos]
-filterUniques ps r = [p | p <- ps, uniqueFilter p r]
+filterLocations :: [Pos] -> Loc -> [Pos]
+filterLocations ps r = [p | p <- ps, locationFilter p r]
 
-uniqueFilter :: Pos -> Loc -> Bool
-uniqueFilter (ls,bs) (r1,r2) = l1 /= r1 && l2 /= r1 && l1 /= r2 && l2 /= r2
+locationFilter :: Pos -> Loc -> Bool
+locationFilter (ls,bs) (r1,r2) = l1 /= r1 && l2 /= r1 && l1 /= r2 && l2 /= r2
                                  where
                                     l1 = fst ls
                                     l2 = snd ls
@@ -189,6 +183,13 @@ neighboursFilter (ls,bs) (r1,r2) = l1 /= r1 && l2 /= r1 && l1 /= r2 && l2 /= r2
                                   where
                                     l1 = fst ls
                                     l2 = snd ls
+
+-- neighboursFilter' :: Pos -> [Int] -> Bool
+-- neighboursFilter' (ls,bs) rs = l1 /= r && l2 /= r
+--                                    where
+--                                     l1 = fst ls
+--                                     l2 = snd ls
+--                                     r <- rs
 
 
 
@@ -204,3 +205,18 @@ solutions2Grids sols w = [(createSolutionGrid s w) | s <- sols]
 createSolutionGrid :: Sol -> Int -> Grid
 createSolutionGrid sol w = chop w [v | (_,v) <- (sortBy (comparing fst) sol)]
 
+
+
+
+
+
+-- neighbours can be found by looking for overlapping positions.
+-- if a possible bone has no overlapping positions with other possible bones, it has no neighbours
+
+-- [[1,2,3],[3,4],[3,4,5,6]]
+-- [(0,1),(0,1),(3,4),(5,6),(3,5)]
+-- putGrid (chop (width startGrid) (concat startGrid))
+
+
+-- filteredPossBones :: [Pos] -> Loc -> [Pos]
+-- filteredPossBones ((l1,l2),(b1,b2)) (r1,r2) = filter (possBoneFilter (l1,l2) (r1,r2)) ((l1,l2),(b1,b2))
